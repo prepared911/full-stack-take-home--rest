d44cd60bb8eadf05dd2cd77082b20ba5bc2a7d6e
amusipatla <amusipatla@gmail.com>
2023-07-29 06:33:20 -0400

    frontend changes for editing description (part 1)

--- console/src/modules/chatroom/ChatroomListItem.tsx
+++ console/src/modules/chatroom/ChatroomListItem.tsx
@@ -1,4 +1,4 @@
-import { KeyboardArrowDown, KeyboardArrowUp } from "@mui/icons-material";
+import { KeyboardArrowDown, KeyboardArrowUp, Edit } from "@mui/icons-material";
 import {
   Box,
   Card,
@@ -7,10 +7,13 @@ import {
   IconButton,
   Typography,
   styled,
+  TextField,
+  Button,
 } from "@mui/material";
 import { useState } from "react";
 
 import { ChatroomTags } from "./ChatroomTags";
+import React from "react";
 
 const ChatroomCard = styled(Card)<CardProps>(({ theme }) => ({
   display: "flex",
@@ -27,8 +30,20 @@ export const ChatroomListItem: React.FC<ChatroomListItemProps> = ({
   chatroom,
 }) => {
   const [showDetails, setShowDetails] = useState(false);
+  const [editDescription, setEditDescription] = useState(false);
+  const [editedDescription, setEditedDescription] = useState(chatroom.description);
 
   const natureCodeName = chatroom.nature_code?.name ?? "Uncategorized";
+
+  const handleCancel = () => {
+    setEditedDescription(chatroom.description); 
+    setEditDescription(false);
+  }
+
+  const handleSave = () => {
+    // TODO: save description to chatroom
+    setEditDescription(false);
+  }
 
   return (
     <ChatroomCard variant="outlined">
@@ -50,10 +65,59 @@ export const ChatroomListItem: React.FC<ChatroomListItemProps> = ({
       </Box>
       <Collapse in={showDetails}>
         <Card sx={{ padding: 2 }}>
-          <Typography variant="body1">Description</Typography>
-          <Typography variant="body2">
-            {chatroom.description ?? "No description provided."}
-          </Typography>
+          <Box
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+            gap="4px"
+          >
+            <Typography variant="body1">Description</Typography>
+            {!editDescription && <IconButton onClick={() => setEditDescription(x => !x)}>
+              <Edit style={{ height: '16px', width: '16px', lineHeight: 1.5}} />
+            </IconButton>}
+          </Box>
+          {editDescription 
+            ? <Box marginTop={1}>
+                <TextField 
+                  size="small"
+                  name="description" 
+                  value={editedDescription} 
+                  fullWidth
+                  multiline
+                  inputProps={{ style: { fontSize:"1rem" }}}
+                  onChange={(e) => setEditedDescription(e.target.value)}
+                />
+                <Box display="flex" justifyContent="flex-end" marginTop={1} gap={1} >
+                  <Button 
+                    size="small"
+                    variant="text"
+                    color="primary"
+                    onClick={handleCancel}
+                  >
+                    Cancel
+                  </Button>
+                  <Button 
+                    size="small"
+                    variant="contained"
+                    color="primary"
+                    type="submit"
+                    // startIcon={
+                    //   isSubmitting ? (
+                    //     <CircularProgress color="inherit" sx={{ fontSize: "1em" }} />
+                    //   ) : null
+                    // }
+                    onClick={handleSave}
+                  >
+                    Save
+                  </Button>
+                </Box>
+              </Box>
+            : 
+              <Typography variant="body2">
+                {chatroom.description ?? "No description provided."}
+              </Typography>
+            
+          }
         </Card>
       </Collapse>
     </ChatroomCard>
