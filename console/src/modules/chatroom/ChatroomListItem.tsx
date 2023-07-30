1ac90ce52779e980ca2bbef72ca3f4730c919e7a
amusipatla <amusipatla@gmail.com>
2023-07-30 14:07:40 -0500

    just formats the code

--- console/src/modules/chatroom/ChatroomListItem.tsx
+++ console/src/modules/chatroom/ChatroomListItem.tsx
@@ -34,11 +34,11 @@ type UpdateChatroomParams = {
 };
 
 async function updateChatroom(params: UpdateChatroomParams): Promise<Chatroom> {
-  console.log({params})
+  console.log({ params });
   const response = await api.patch<Chatroom>(`/chatrooms/${params.id}`, {
     description: params.description,
   });
-  console.log({response})
+  console.log({ response });
   return response.data;
 }
 
@@ -47,24 +47,29 @@ export const ChatroomListItem: React.FC<ChatroomListItemProps> = ({
 }) => {
   const [showDetails, setShowDetails] = useState(false);
   const [editDescription, setEditDescription] = useState(false);
-  const [editedDescription, setEditedDescription] = useState(chatroom.description);
+  const [editedDescription, setEditedDescription] = useState(
+    chatroom.description
+  );
   const [loadingSave, setLoadingSave] = useState(false);
 
   const natureCodeName = chatroom.nature_code?.name ?? "Uncategorized";
 
   const handleCancel = () => {
-    setEditedDescription(chatroom.description); 
+    setEditedDescription(chatroom.description);
     setEditDescription(false);
-  }
+  };
 
   const handleSave = async () => {
     setLoadingSave(true);
-    console.log({chatroom});
-    const updatedChatroom =  await updateChatroom({id: chatroom.id, description: editedDescription});
+    console.log({ chatroom });
+    const updatedChatroom = await updateChatroom({
+      id: chatroom.id,
+      description: editedDescription,
+    });
     chatroom.description = updatedChatroom.description;
     setLoadingSave(false);
     setEditDescription(false);
-  }
+  };
 
   return (
     <ChatroomCard variant="outlined">
@@ -86,59 +91,65 @@ export const ChatroomListItem: React.FC<ChatroomListItemProps> = ({
       </Box>
       <Collapse in={showDetails}>
         <Card sx={{ padding: 2 }}>
-          <Box
-            display="flex"
-            flexDirection="row"
-            alignItems="center"
-            gap="4px"
-          >
+          <Box display="flex" flexDirection="row" alignItems="center" gap="4px">
             <Typography variant="body1">Description</Typography>
-            {!editDescription && <IconButton onClick={() => setEditDescription(x => !x)}>
-              <Edit style={{ height: '16px', width: '16px', lineHeight: 1.5}} />
-            </IconButton>}
-          </Box>
-          {editDescription 
-            ? <Box marginTop={1}>
-                <TextField 
-                  size="small"
-                  name="description" 
-                  value={editedDescription} 
-                  fullWidth
-                  multiline
-                  inputProps={{ style: { fontSize:"1rem" }}}
-                  onChange={(e) => setEditedDescription(e.target.value)}
+            {!editDescription && (
+              <IconButton onClick={() => setEditDescription((x) => !x)}>
+                <Edit
+                  style={{ height: "16px", width: "16px", lineHeight: 1.5 }}
                 />
-                <Box display="flex" justifyContent="flex-end" marginTop={1} gap={1} >
-                  <Button 
-                    size="small"
-                    variant="text"
-                    color="primary"
-                    onClick={handleCancel}
-                  >
-                    Cancel
-                  </Button>
-                  <Button 
-                    size="small"
-                    variant="contained"
-                    color="primary"
-                    type="submit"
-                    startIcon={
-                      loadingSave ? (
-                        <CircularProgress color="inherit" sx={{ fontSize: "1em" }} />
-                      ) : null
-                    }
-                    onClick={handleSave}
-                  >
-                    Save
-                  </Button>
-                </Box>
+              </IconButton>
+            )}
+          </Box>
+          {editDescription ? (
+            <Box marginTop={1}>
+              <TextField
+                size="small"
+                name="description"
+                value={editedDescription}
+                fullWidth
+                multiline
+                inputProps={{ style: { fontSize: "1rem" } }}
+                onChange={(e) => setEditedDescription(e.target.value)}
+              />
+              <Box
+                display="flex"
+                justifyContent="flex-end"
+                marginTop={1}
+                gap={1}
+              >
+                <Button
+                  size="small"
+                  variant="text"
+                  color="primary"
+                  onClick={handleCancel}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  size="small"
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  startIcon={
+                    loadingSave ? (
+                      <CircularProgress
+                        color="inherit"
+                        sx={{ fontSize: "1em" }}
+                      />
+                    ) : null
+                  }
+                  onClick={handleSave}
+                >
+                  Save
+                </Button>
               </Box>
-            : 
-              <Typography variant="body2">
-                {chatroom.description ?? "No description provided."}
-              </Typography>
-            
-          }
+            </Box>
+          ) : (
+            <Typography variant="body2">
+              {chatroom.description ?? "No description provided."}
+            </Typography>
+          )}
         </Card>
       </Collapse>
     </ChatroomCard>
